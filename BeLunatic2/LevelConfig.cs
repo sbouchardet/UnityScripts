ee15109d823efe75ceb66ed219c78e2c07bb32e5
Susana Bouchardet <susana.bouchardet@bouchardet.local>
2016-10-29 18:10:44 -0200

    +1

--- BeLunatic2/LevelConfig.cs
+++ BeLunatic2/LevelConfig.cs
@@ -4,107 +4,146 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using System;
 
-
 [System.Serializable]
-public class LevelDemand : IEquatable<LevelDemand> {
+public class Demand{
+	public string Enemy;
+	public int Amount;
 
-	public int cont;
-	public GameObject type;
-
-
-	public LevelDemand(GameObject t, int c){
-		this.cont = c;
-		this.type = t;
+	public Demand (string Enm, int a = -1){
+		this.Enemy = Enm;
+		this.Amount = a;
 	}
-
-	public bool Equals(LevelDemand other)
-	{	
-		if (this.type==null || other.type == null)
-			return false;
-		if (!type.name.Equals(other.type.name) )return false;
 		
-		return true;
+
+	public override string ToString () {
+		return string.Format ("[LevelConfig]"+this.Enemy+" [ "+this.Amount+" ]");
 	}
 
-	public override string ToString ()
+	public override bool Equals (object obj)
 	{
-		return string.Format (this.type.name+" - "+cont.ToString());
+		Demand other = obj as Demand;
+		if (other.Enemy != null && this.Enemy != null)
+			return this.Enemy == other.Enemy;
+		return false;
 	}
+		
 }
+
+
+
 
 public class LevelConfig : MonoBehaviour {
 
-	public List<LevelDemand> Demands = new List<LevelDemand>();
+	//FIXME: need refactoring!
+
+	public List<Demand> ListOfDemands = new List<Demand> ();
+
+	private List<Demand> EnemysAtLevel = new List<Demand> ();
+
 	public Font NumberFont;
 	public Color ColorNumber = new Color(1,1,1,1);
 	public Font LetterFont;
 	public Color ColorLetter = new Color(1,1,1,1);
 	public int FontSize = 20;
 
-	private GUIStyle guiStyle = new GUIStyle(); 
+	private GUIStyle guiStyle = new GUIStyle();
 
-	private List<LevelDemand> InitialEnemys = new List<LevelDemand>();
-
-
-	// Use this for initialization
 	void Start () {
 
 		GameObject[] allEnemys = GameObject.FindGameObjectsWithTag ("enemy");
-		foreach (GameObject i in allEnemys) {
-			if (InitialEnemys.Contains (new LevelDemand (i, 0))) {
-				InitialEnemys [InitialEnemys.IndexOf (new LevelDemand (i, 0))].cont += 1;
+		for (int i = 0; i < allEnemys.Length; i++) {
+			GameObject enemy = allEnemys [i];
+			if (EnemysAtLevel.Contains (new Demand (enemy.name))) {
+				EnemysAtLevel [EnemysAtLevel.IndexOf (new Demand(enemy.name,1))].Amount += 1;
 			} else {
-				InitialEnemys.Add (new LevelDemand (i, 1));
+				EnemysAtLevel.Add (new Demand (enemy.name, 1));
 			}
 		}
-	
+
 		}
 	
-	// Update is called once per frame
 	void Update () {
-		UpdateEnemyInfo ();
+		string abducted = WhoWasAbducted ();
+		if (abducted != "nobody") {
+			UpdateListOfDemands (abducted);
+		}
+			
 	}
 
-	void OnGUI()
-	{
+	void OnGUI(){
 
 		guiStyle.fontSize = FontSize;
-		foreach (LevelDemand a in Demands){	
+		foreach (Demand a in ListOfDemands){	
 			GUI.skin.font = LetterFont;
 			GUI.color = ColorLetter;
-			GUI.Label(new Rect((Screen.width)*(3f/4f), 10, Screen.width*(3.75f/4f), 40), a.type.name+":",guiStyle);
+			GUI.Label(new Rect((Screen.width)*(3f/4f), 10, Screen.width*(3.75f/4f), 40), a.Enemy+":",guiStyle);
 			GUI.skin.font = NumberFont;
 			GUI.color = ColorNumber;
-			GUI.Label(new Rect((Screen.width)*(3.75f/4f), 10, Screen.width, 40), a.cont.ToString(),guiStyle);
+			GUI.Label(new Rect((Screen.width)*(3.75f/4f), 10, Screen.width, 40), a.Amount.ToString(),guiStyle);
 		}
 
 
 	}
+		
 
-	void UpdateEnemyInfo(){
 
-		List <LevelDemand> localGO = new List<LevelDemand> ();
-		GameObject[] allEnemys = GameObject.FindGameObjectsWithTag ("enemy");
+	private void AdjustListDemands(List<Demand> d){
+		for (int i=0;i<d.Count;i++){
+			if (d[i].Amount == 0)
+				d.RemoveAt (i);
+		}
+	}
+		
+	private void PrintListDemands(List<Demand> d){
+		if (d.Count == 0) {
+			Debug.Log (" ---ListDemand--- ");
+			Debug.Log ("  ! EMPTY LIST !  ");
+			Debug.Log (" ---------------- ");
+			return;
+		} else {
 
-		foreach (GameObject i in allEnemys) {
-			if (localGO.Contains (new LevelDemand (i, 0))) {
-				localGO [localGO.IndexOf (new LevelDemand (i, 0))].cont += 1;
+			Debug.Log (" ---ListDemand--- ");
+			foreach (Demand i in d)
+				Debug.Log (i.ToString ());
+			Debug.Log (" ---------------- ");
+		}
+	}
+
+
+
+	private string WhoWasAbducted(){
+		
+		List<Demand> enemyCountReduced = new List<Demand> ();
+		GameObject[] enemysNow = GameObject.FindGameObjectsWithTag ("enemy");
+
+		for (int i = 0; i < enemysNow.Length; i++) {
+			GameObject enemy = enemysNow [i];
+			if (enemyCountReduced.Contains (new Demand (enemy.name))) {
+				enemyCountReduced [enemyCountReduced.IndexOf (new Demand(enemy.name,1))].Amount += 1;
 			} else {
-				localGO.Add (new LevelDemand (i, 1));
+				enemyCountReduced.Add (new Demand (enemy.name, 1));
+			}
+		}
+	
+		if (enemyCountReduced.Count == 0) {
+			EnemysAtLevel.Clear();
+		}
+		for (int i = 0; i < enemyCountReduced.Count; i++) {
+//			Debug.Log (EnemysAtLevel [i].Enemy);
+//			Debug.Log (enemyCountReduced [i].Enemy);
+			Demand InfoEnemyUntilNow = EnemysAtLevel [EnemysAtLevel.IndexOf (new Demand(enemyCountReduced [i].Enemy))];
+//			Debug.Log (InfoEnemyUntilNow);
+			if (InfoEnemyUntilNow.Amount > enemyCountReduced [i].Amount) {
+				EnemysAtLevel [EnemysAtLevel.IndexOf (enemyCountReduced [i])].Amount = enemyCountReduced [i].Amount;
+				return enemyCountReduced [i].Enemy;
 			}
 		}
 
-		 //Falta arrumar a exibição da contagem de abduções
-		foreach (LevelDemand i in Demands) {
-			i.cont = i.cont -  
-				(InitialEnemys [InitialEnemys.IndexOf (i)].cont - 
-					localGO [localGO.IndexOf (i)].cont);
-
-			Debug.Log (InitialEnemys [InitialEnemys.IndexOf (i)].cont);
-				Debug.Log (localGO [localGO.IndexOf (i)].cont);
-		}
-
+		return "nobody";
 	}
 
+	private void UpdateListOfDemands(string name){
+		ListOfDemands [ListOfDemands.IndexOf (new Demand (name))].Amount -= 1;
+	}
 
 }
