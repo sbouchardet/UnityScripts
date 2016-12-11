fd09d87127a81ae3c0568f93f04e4803341d8679
Susana Bouchardet <susana.bouchardet@bouchardet.local>
2016-12-11 00:23:47 -0200

    +1

---
+++ BeLunatic2/JumpPlataform.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpPlataform : MonoBehaviour {
+
+	void OnControllerColliderHit(ControllerColliderHit hit){
+		if (hit.transform.gameObject.name == "plataforma") {
+			this.transform.parent = hit.transform;
+			Debug.Log ("Temos um jogo");
+		} else {
+			this.transform.parent = null;
+
+		}
+	}
+}
