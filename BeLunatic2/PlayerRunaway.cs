dbfb4000197817b166ed13d5273dcf69bdd98bf6
Susana Bouchardet <susana.bouchardet@bouchardet.local>
2016-12-12 00:37:05 -0200

    +1

---
+++ BeLunatic2/PlayerRunaway.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRunaway : MonoBehaviour {
+
+	public GameObject EndPoint;
+	public float Distance = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (EndPoint.transform.position, transform.position) < Distance) {
+			GameManager gm = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager> ();
+			gm.loadNextLevel ();
+		}
+	}
+}
