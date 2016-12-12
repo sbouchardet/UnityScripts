4ed4ade9a21dd86e24881b36c862988a83a749a7
Susana Bouchardet <susana.bouchardet@bouchardet.local>
2016-12-12 00:27:23 -0200

    +1

--- BeLunatic2/BossChase.cs
+++ BeLunatic2/BossChase.cs
@@ -4,31 +4,27 @@ using System.Collections;
 public class BossChase : WalkOnPoints {
 
 	public float damageRange = 10;
-	public float SecondsBetweenDamages = 1;
-	public int damage = 10;
 
 	private GameObject player;
+	private GameObject gm;
+
 	private float seconds = 0;
 	public override void StartWalk() {
 		player = GameObject.FindGameObjectWithTag ("Player");
+		gm = GameObject.FindGameObjectWithTag ("GameManager");
+
 	}
 	
 	// Update is called once per frame
 	public override void UpdateWalk ()  {
 		Walk (false);
 		if (Vector3.Distance (transform.position, player.transform.position) <= damageRange) {
-			seconds += Time.deltaTime;
-			if (seconds > SecondsBetweenDamages) {
 				hit ();
-				seconds = 0;
-			}
-		} else {
-			seconds = 0;
-		}
+
+		} 
 	}
 
 	public void hit(){
-		player.GetComponent<PlayerRunaway> ().Energy -= damage;
-		Debug.Log (player.GetComponent<PlayerRunaway> ().Energy);
+		gm.GetComponent<GameManager> ().ShowGameOver ();
 	}
 }
